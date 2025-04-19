f9a116b2150dfc30efd5629bace6035813908a03
funnymonke0 <aivectorec@gmail.com>
2025-04-19 00:24:52 -0400

    monkebot v5
    sleeker bot class with less convoluted error handling
    
    incorporates proxy list that allows for proxies with authentication, rotating proxies, and proxies without authentication
    
    allows for room selection if no room code is inputted
    
    saved console log for manager class

--- src/bombpartybot.py
+++ src/bombpartybot.py
@@ -9,6 +9,7 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException,
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import random
+import re
 from time import sleep
 import logging
 
@@ -63,6 +64,7 @@ class BPB():
         
 
         ##settings
+        self.maxWait = 5
         # self.selectMode = 'long'
         self.selectMode = 'short'
         # self.selectMode = 'avg'
@@ -75,27 +77,13 @@ class BPB():
         self.typeDelay = 0.12
         self.randomness = 0.07
 
-        self.tickSpeed = 0
+        self.tickSpeed = 0.05
 
-        self.ref = dicts
         self.dicts = dicts
 
         self.joinedRoom = self.joinRoom(roomCode)
 
     
-
-    def procTerminated(self, message = '', verbose = True, pid = '', ret = False):
-        if verbose:
-            self.console.error(f'Error {message} occured in method {pid}.')
-        return ret
-    
-
-    def procSuccess(self, message = '', verbose = False, pid = '', ret = True):
-        if verbose:
-            self.console.info(f'method {pid} completed {message} sucessfully.')
-        return ret
-
-
     def findSuffix(self, suffix):
         lst = []
         try:
@@ -103,144 +91,100 @@ class BPB():
                 if suffix.lower() in wrd:
                     lst.append(wrd)
             lst.sort(key= lambda x: len(x))
-            return self.procSuccess(ret=lst)
+
+            return lst
         except IndexError:
             pass
-            return self.procTerminated(message='could not find suffix !', ret=None)
-        
-    
-    
-    def waitForLoad(self):
-        browser = self.driver
-        try:
-            WebDriverWait(browser, 10)
-            return self.procSuccess()   
-        except TimeoutException:
-            pass
-            return self.procTerminated("page took too long to load.")
-         
+            return None        
     
 
-    def locate(self, locator, lmd, name = 'element', delay = 5, verbose = False):
+    def locate(self, locator, lmd, delay = None):
+        if not delay:
+            delay = self.maxWait
         browser = self.driver
 
         wait = WebDriverWait(browser, delay, poll_frequency=self.tickSpeed)
         try:
             out = wait.until(lmd(locator))
-            return self.procSuccess(pid= 'locate', message=f'find {name}', ret=out, verbose=verbose)
+            return out
+
         except TimeoutException:
             pass
-            return self.procTerminated(f"could not find element {name} !", pid= 'locate', verbose=verbose)
+        return None
 
 
-    def joinRoom(self, roomCode):
+    def joinRoom(self, roomCode=None):
         browser = self.driver
-        try:
-            browser.get("https://jklm.fun/"+roomCode)
-        except ConnectionError:
-            pass
-            return self.procTerminated(pid = 'joinRoom', message='unable to join room !')
-            
+        browser.get("https://jklm.fun/"+roomCode)
         if roomCode == '':
             self.console.info('waiting for room selection')
             while True:
                 try:
                     if WebDriverWait(browser, self.tickSpeed, ignored_exceptions=TimeoutException).until(EC.url_matches("^((https|http)\:\/\/jklm\.fun\/)[A-Z][A-Z][A-Z][A-Z]")):
+                        
                         break
-                except:
+                except TimeoutException:
                     pass
-
-        if not self.waitForLoad():
-            return self.procTerminated(pid = 'joinRoom')
         
-        textbox = self.locate((By.XPATH, '//input[@class = "styled nickname"]'), name = 'nickname textbox', lmd = self.visibleCondition, verbose=True)
-        if not textbox:
-            return self.procTerminated(pid = 'joinRoom', message='unable to find nickname box !')
+        WebDriverWait(browser, self.maxWait)
+        self.roomCode = browser.current_url.split('/',1)[1]
 
-        submit = self.locate((By.XPATH, "/html/body/div[2]/div[3]/form/div[2]/button"), name = 'name submit button', lmd = self.clickableCondition, verbose=True)
-        if not submit:
-            return self.procTerminated(pid = 'joinRoom', message='unable to find ok button !')
+        textbox = self.locate((By.XPATH, '//input[@class = "styled nickname"]'), lmd = self.visibleCondition, delay=self.maxWait)
+
+        submit = self.locate((By.XPATH, "/html/body/div[2]/div[3]/form/div[2]/button"), lmd = self.clickableCondition, delay=self.maxWait)
         
         textbox.send_keys(Keys.CONTROL + Keys.BACKSPACE)
         sleep(0.1)
         textbox.send_keys(self.username)
         submit.click()
+        WebDriverWait(browser, self.maxWait)
 
-        if not self.waitForLoad():
-            return self.procTerminated(pid = 'joinRoom')
-        
-        return self.procSuccess(pid='joinRoom')
 
     def updateIframe(self):
         browser = self.driver
         browser.switch_to.default_content()
-        iFrame = self.locate((By.XPATH, "//iframe[contains(@src, 'bombparty')]"), name = 'game iframe',lmd= self.presenceCondition)
-        if iFrame:
+        iFrame = self.locate((By.XPATH, "//iframe[contains(@src, 'bombparty')]"), lmd= self.presenceCondition,delay=self.tickSpeed)
+        browser.switch_to.frame(iFrame)
 
-            browser.switch_to.frame(iFrame)
-            return self.procSuccess(pid = 'updateIframe')
-        return self.procTerminated(pid = 'updateIframe', message= 'unable to update game frame !')
 
-    def join(self, sensitive = False):
-
-        delay = self.tickSpeed
-        if sensitive:
-            delay = 5
-        join = self.locate((By.XPATH, "//button[@class = 'styled joinRound']"), name='join button',delay= delay, lmd = self.clickableCondition, verbose=sensitive)
+    def join(self, delay):
+        join = self.locate((By.XPATH, "//button[@class = 'styled joinRound']"), delay= delay, lmd = self.clickableCondition)
         if join:
             join.click()
-            return self.procSuccess(pid='join')
-
-        autojoin = self.locate((By.XPATH, "//button[@class = 'autojoinButton styled']"), name='autojoin button',delay=delay, lmd = self.clickableCondition, verbose=sensitive)
-        if autojoin:
-            autojoin.click()
-            return self.procSuccess(pid='join')
-        
-
-        return self.procTerminated(pid='join', message='could not join round !', verbose=sensitive)
-
-    
         
     def updateLoop(self):
         browser = self.driver
         browser.switch_to.default_content()
-        disconnected = self.locate((By.XPATH, '//div[@class = "disconnected page"]'), name = 'disconnected page', delay= self.tickSpeed, lmd = self.visibleCondition)
+        disconnected = self.locate((By.XPATH, '//div[@class = "disconnected page"]'), delay= self.tickSpeed, lmd = self.visibleCondition)
         if disconnected:
-            return self.procTerminated(pid = 'updateLoop', message = 'disconnected from room !')
-        if not self.updateIframe():
-            return self.procTerminated(pid='updateLoop')
-        lastRound = self.locate((By.XPATH, '//div[@class = "lastRound"]'), name = 'last round', delay= self.tickSpeed, lmd = self.visibleCondition)
+            browser.quit()
+            return False
+        self.updateIframe()
+        
+        lastRound = self.locate((By.XPATH, '//div[@class = "lastRound"]'), delay= self.tickSpeed, lmd = self.visibleCondition)
         if lastRound:
-            self.join()
-            self.console.info(f'used {len(self.ref)-len(self.dicts)} words out of {len(self.ref)}')
+            self.join(self.maxWait)
             self.console.info('reloading dictionary')
-            self.dicts = self.ref
+            self.dicts = self.dicts+self.replace
+            self.console.info(f'used {len(self.replace)} words out of {len(self.dicts)}')
+            self.replace= []
+        return True
             
-        return self.procSuccess(pid='updateLoop')
+
     
     def botLoop(self):
 
-        if not self.joinedRoom:
-            return self.procTerminated("must first join a room !", pid = 'botLoop')
-        if not self.dicts:
-            return self.procTerminated("must first load dicts !", pid = 'botLoop')
-        
-        if not self.updateIframe():
-            return self.procTerminated(pid = 'botLoop')
-        if not self.join(sensitive=True):
-            return self.procTerminated(pid = 'botLoop')    
-
-        while True:
-            if not self.updateLoop():
-                self.console.info("failed to complete update loop ! gracefully terminating...")
-                return self.procSuccess(pid='botLoop')
+        self.replace = []
+        self.updateIframe()
+        self.join(self.maxWait)
+        while self.updateLoop():
 
             ans = None
-            
-            syllable = self.locate((By.XPATH, "//div[@class = 'syllable']"), name ='active syllable', delay=self.tickSpeed, lmd = self.visibleCondition)
-            if syllable:  
+            syllable = self.locate((By.XPATH, "//div[@class = 'syllable']"), delay=self.tickSpeed, lmd = self.visibleCondition)
+            textbox = self.locate((By.XPATH, '//form//input[@maxlength = "30"]'), delay=self.tickSpeed, lmd = self.visibleCondition)
+            if textbox and syllable:
                 syllable = syllable.text
-                
+                self.console.info(f'found syllable: {syllable}')
                 lst = self.findSuffix(syllable)
 
                 if lst:
@@ -250,31 +194,26 @@ class BPB():
                         ans = lst[round((len(lst)-1)/2)]
                     elif self.selectMode == 'short':
                         ans = lst[0]
-
-            textbox = self.locate((By.XPATH, '//form//input[@maxlength = "30"]'), name = 'game input', delay=self.tickSpeed, lmd = self.visibleCondition)
-            if textbox:
                 if ans:
-                    self.console.info(f'found syllable: {syllable}')
+                    
                     self.console.info(f'found answer: {ans}')
                     if self.cyberbullying:
                         try:
-                            textbox.send_keys(ans) ##if you wanna get hackusated
-                            textbox.send_keys(Keys.ENTER)
-                        except:
+                            textbox.send_keys(ans+Keys.ENTER) ##if you wanna get hackusated
+                        except ElementNotInteractableException:
                             pass
                     else:
-                        sleep(self.offset)
-                        self.simType(textbox, ans)
-                        textbox.send_keys(Keys.ENTER)
+                        
+                        self.simType(textbox, ans+Keys.ENTER)
                     
+                    self.replace.append(ans)
                     self.dicts.remove(ans)
                 else:
                     self.console.error("syllable not in dictionary !")
                     textbox.send_keys('/suicide'+Keys.ENTER)
-            else:
-                self.console.info('waiting for turn...')
 
     def simType(self, obj, txt):
+        sleep(self.offset)
         txtArr = list(txt)
         for letter in txtArr:
             obj.send_keys(letter)
@@ -294,6 +233,9 @@ class BotManager():
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
 
+        fh = logging.FileHandler('BotManager.log')
+        fh.setLevel(logging.DEBUG)
+
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         ch.setFormatter(formatter)
 
@@ -305,7 +247,7 @@ class BotManager():
         self.proxy = None
         self.proxyList = self.loadFromFile(proxyFile)
 
-        self.genUsername = lambda x, y: x+str(random.randint(0,int('9'*y)))
+        self.genUsername = lambda x, y: x+str(random.randint(1,9))+str(random.randint(0,9))*(y-1)
 
 
     def loadDicts(self, dictUrls):
@@ -329,7 +271,7 @@ class BotManager():
 
         if username == '':
             username = self.genUsername('Guest', 4)
-            # username = self.genUsername('monkebotv4 #', 4)
+            # username = self.genUsername('monkebotv5 #', 4)
         if self.proxy:
             addr, port, user, pswd = self.loadProxy(self.proxy)
 
@@ -348,18 +290,27 @@ class BotManager():
         proxyNo = 0
         bot = None
         proxyList = self.proxyList
-
+        retries = 0
+        thresh = 2
         while proxyNo < len(proxyList):
             if not bot:
-                self.proxy = proxyList[proxyNo]
-                bot = self.botInit()
-                if bot.botLoop():
-                    self.console.info('internal bot error occurred; bot terminated gracefully')
+                if retries <= thresh:
+                    self.proxy = proxyList[proxyNo]
+                    bot = self.botInit()
+                    try:
+                        bot.botLoop()
+                        bot = None
+                        retries = 0
+                        proxyNo+=1
+                        
+                    except Exception as s:
+                        self.console.info(f'Exception {s} in Bot {bot.username}; retrying')
+                        bot = None
+                        retries += 1
                 else:
-                    self.console.error('Something went wrong.')
-                proxyNo+=1
-                bot = None
-
+                    retries = 0
+                    proxyNo+=1    
+        bot = None
         self.console.info('goodbye!')
 
     def loadFromFile(self, filename):
@@ -368,9 +319,11 @@ class BotManager():
                 with open(filename, 'r') as file:
                     proxyList = file.readlines()
                 f = "\n".join([x for x in proxyList])
-                self.console.info(f'proxies found {f}')
-                return proxyList
-            
+                ex = "(((25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|[1-9])\.){3}(25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|[1-9])|(\w*\.\w*\.\w*))\:(6553[0-5]|655[0-2]\d|65[0-4]\d\d|6[0-4]\d\d\d|[1-5]\d\d\d|[1-9]\d\d\d|[1-9]\d\d|[1-9]\d|[1-9])(\:.*\:.*)?"
+                if re.match(ex,f):
+                    self.console.info(f'proxies found {f}')
+                    return proxyList
+        
             except FileNotFoundError:
                 self.console.error('cannot find proxy file; using localhost')
                 return [None]
@@ -383,8 +336,6 @@ class BotManager():
             return split[0], split[1], split[2], split[3]
         elif len(split) == 2:
             return split[0], split[1], None, None
-        else:
-            raise Exception().add_note('cannot use loadProxy in this context; possible format error in proxy list')
         
 if __name__ == "__main__" :
 
@@ -400,7 +351,6 @@ if __name__ == "__main__" :
         "https://pastebin.com/raw/M6TZ8Uxy",
         ]
     link = str(input("paste code: ")).upper()
-    # link = "nfue" hardcoded test room
     name = str(input("username: "))
 
 
