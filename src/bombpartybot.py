7ae5c79d123bd46cdf793fdd8b7c8d1e46a80fe5
funnymonke0 <aivectorec@gmail.com>
2025-04-19 20:08:51 -0400

    tweaks
    added smart typing
    better error handling
    manager log
    need to add realistic typing

--- src/bombpartybot.py
+++ src/bombpartybot.py
@@ -16,8 +16,17 @@ import logging
 
 
 class BPB():
-    def __init__(self, dicts, username, roomCode, proxy = None):
+    def __init__(self, dicts, username, roomCode=None, proxy=None):
+
+        self.visibleCondition = lambda x: EC.visibility_of_element_located(x)
+        self.clickableCondition = lambda x: EC.element_to_be_clickable(x)
+        self.presenceCondition = lambda x: EC.presence_of_element_located(x)
+        self.genUsername = lambda x, y: x+str(random.randint(1,int('9'*y)))
+
         self.username = username
+        if not username:
+            self.username = self.genUsername('Guest', 4)
+
         self.console = logging.getLogger(f'{self.username} CONSOLE')
         self.console.setLevel(logging.DEBUG)
 
@@ -28,27 +37,18 @@ class BPB():
         ch.setFormatter(formatter)
 
         self.console.addHandler(ch)
-        
-        
-        ##const
 
-        self.visibleCondition = lambda x: EC.visibility_of_element_located(x)
-        self.clickableCondition = lambda x: EC.element_to_be_clickable(x)
-        self.presenceCondition = lambda x: EC.presence_of_element_located(x)
+
         
+
         chrome_options = ChromeOptions()
         service = ChromeService()
         seleniumwire_options = {
             }
 
-        ##extra options
 
-
-        self.username = username
         self.roomCode = roomCode
 
-
-        
         if proxy:
             seleniumwire_options ['proxy'] = {
                 'https': proxy,
@@ -59,22 +59,22 @@ class BPB():
         else:
             proxy = 'localhost'
         self.driver = webdriver.Chrome(service=service, options=chrome_options, seleniumwire_options=seleniumwire_options)
-        self.console.info(f'initialized bot {username} running @ {proxy}')
+        self.console.info(f'initialized bot {self.username} running @ {proxy}')
 
         
 
         ##settings
         self.maxWait = 5
         # self.selectMode = 'long'
-        self.selectMode = 'short'
-        # self.selectMode = 'avg'
+        # self.selectMode = 'short'
+        self.selectMode = 'smart'
 
         # self.cyberbullying = True
 
         self.cyberbullying = False
         self.offset = 0.6
 
-        self.typeDelay = 0.12
+        self.typeDelay = 0.10
         self.randomness = 0.07
 
         self.tickSpeed = 0.05
@@ -88,7 +88,7 @@ class BPB():
         lst = []
         try:
             for wrd in self.dicts:
-                if suffix.lower() in wrd:
+                if suffix.lower() in wrd.lower():
                     lst.append(wrd)
             lst.sort(key= lambda x: len(x))
 
@@ -103,7 +103,7 @@ class BPB():
             delay = self.maxWait
         browser = self.driver
 
-        wait = WebDriverWait(browser, delay, poll_frequency=self.tickSpeed)
+        wait = WebDriverWait(browser, delay+self.tickSpeed, poll_frequency=self.tickSpeed) #always at least 1 check
         try:
             out = wait.until(lmd(locator))
             return out
@@ -113,21 +113,21 @@ class BPB():
         return None
 
 
-    def joinRoom(self, roomCode=None):
+    def joinRoom(self, roomCode):
         browser = self.driver
         browser.get("https://jklm.fun/"+roomCode)
-        if roomCode == '':
+
+        if not roomCode:
             self.console.info('waiting for room selection')
             while True:
                 try:
-                    if WebDriverWait(browser, self.tickSpeed, ignored_exceptions=TimeoutException).until(EC.url_matches("^((https|http)\:\/\/jklm\.fun\/)[A-Z][A-Z][A-Z][A-Z]")):
-                        
+                    if WebDriverWait(browser, self.tickSpeed).until(EC.url_matches("^((https|http)\:\/\/jklm\.fun\/)[A-Z][A-Z][A-Z][A-Z]")):
                         break
                 except TimeoutException:
                     pass
         
         WebDriverWait(browser, self.maxWait)
-        self.roomCode = browser.current_url.split('/',1)[1]
+        
 
         textbox = self.locate((By.XPATH, '//input[@class = "styled nickname"]'), lmd = self.visibleCondition, delay=self.maxWait)
 
@@ -164,12 +164,9 @@ class BPB():
         lastRound = self.locate((By.XPATH, '//div[@class = "lastRound"]'), delay= self.tickSpeed, lmd = self.visibleCondition)
         if lastRound:
             self.join(self.maxWait)
-            self.console.info('reloading dictionary')
             self.dicts = self.dicts+self.replace
-            self.console.info(f'used {len(self.replace)} words out of {len(self.dicts)}')
             self.replace= []
         return True
-            
 
     
     def botLoop(self):
@@ -190,10 +187,13 @@ class BPB():
                 if lst:
                     if self.selectMode == 'long':
                         ans = lst[len(lst)-1]
-                    elif self.selectMode == "avg":
-                        ans = lst[round((len(lst)-1)/2)]
                     elif self.selectMode == 'short':
                         ans = lst[0]
+                    elif self.selectMode == 'smart':
+                        ans = lst[0]
+                        if len(lst[len(lst)-1])>20:
+                            ans = lst[len(lst)-1]
+
                 if ans:
                     
                     self.console.info(f'found answer: {ans}')
@@ -225,7 +225,7 @@ class BPB():
 
 class BotManager():
     #manage bot persistence, proxies and other settings, etc.
-    def __init__(self, dictUrls, roomCode = '', proxyFile = '', username = ''):
+    def __init__(self, dictFile, roomCode = None, proxyFile = None, username = None):
         
         self.console = logging.getLogger('MANAGER CONSOLE')
         self.console.setLevel(logging.DEBUG)
@@ -240,38 +240,57 @@ class BotManager():
         ch.setFormatter(formatter)
 
         self.console.addHandler(ch)
-
-        self.dicts = self.loadDicts(dictUrls)
+        self.console.addHandler(fh)
+        
         self.roomCode = roomCode
         self.username = username
         self.proxy = None
-        self.proxyList = self.loadFromFile(proxyFile)
+        self.proxyList = [None]
+        self.dicts = []
+        
+        
+        proxyRegex = "(((25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|[1-9])\.){3}(25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|[1-9])|(\w*\.\w*\.\w*))\:(6553[0-5]|655[0-2]\d|65[0-4]\d\d|6[0-4]\d\d\d|[1-5]\d\d\d|[1-9]\d\d\d|[1-9]\d\d|[1-9]\d|[1-9])(\:.*\:.*)?"
+        if proxyFile:
+            self.proxyList = self.loadFromFile(proxyFile, proxyRegex)
+            self.console.info(f"loaded {len(self.proxyList)} proxies from {proxyFile}")
 
-        self.genUsername = lambda x, y: x+str(random.randint(1,9))+str(random.randint(0,9))*(y-1)
+        dictUrls = self.loadFromFile(dictFile, "^((https|http)\:\/\/)((\w*\.\w*\.\w*)|(\w*\.\w*))((\/\w*(\.\w*)?)*)?")
+
+        if dictUrls:
+            self.console.info("loading dicts from urls")
+            self.dicts += self.loadUrls(dictUrls)
+
+        dictPlainText = self.loadFromFile(dictFile, "^\w*$")
+        if dictPlainText:
+            self.console.info("loading dicts from plaintext")
+            self.dicts += dictPlainText
+        self.dicts = list(set(self.dicts))
+        self.console.info(f"loaded {len(self.dicts)} words from {dictFile}")
 
 
-    def loadDicts(self, dictUrls):
-        #use selenium for this?
+    def loadUrls(self, dictUrls):
+        service = ChromeService()
+        options = ChromeOptions()
+        options.add_argument('--headless=new')
+        browser = webdriver.Chrome(service=service, options=options)
+        
         dicts = []
         for url in dictUrls:
             try:
-                r = requests.get(url)
-                soup = BeautifulSoup(r.content, 'html.parser')
-                dicts += [*soup.get_text().lower().split()]
+                browser.get(url)
+                WebDriverWait(browser, 5)
+                content = browser.find_element(By.XPATH, '//pre').text.lower()
+                dicts += content.split('\n')
             except:
+                self.console.info(f'Cannot get url {url}')
                 pass
-                return None
-            
-        dicts = list(set(dicts))
 
         return dicts
 
     def botInit(self):
+        fProxy = None 
         username = self.username
 
-        if username == '':
-            username = self.genUsername('Guest', 4)
-            # username = self.genUsername('monkebotv5 #', 4)
         if self.proxy:
             addr, port, user, pswd = self.loadProxy(self.proxy)
 
@@ -279,11 +298,9 @@ class BotManager():
             if user and pswd:
                 fProxy = f"https://{user}:{pswd}@{addr}:{port}"
 
-            bot = BPB(dicts=self.dicts, proxy=fProxy, username=username, roomCode=self.roomCode)
-        else:
-            bot = BPB(dicts=self.dicts, username=username, roomCode=self.roomCode)
-        
+        bot = BPB(dicts=self.dicts, proxy=fProxy, username=username, roomCode=self.roomCode)
         return bot
+        
     
     
     def persistLoop(self):
@@ -299,36 +316,38 @@ class BotManager():
                     bot = self.botInit()
                     try:
                         bot.botLoop()
+                        self.console.info(f'Bot {bot.username} disconnected successfully')
                         bot = None
                         retries = 0
                         proxyNo+=1
                         
                     except Exception as s:
-                        self.console.info(f'Exception {s} in Bot {bot.username}; retrying')
+                        self.console.info(f'Exception {s} in Bot {bot.username}; retrying with proxy {self.proxy}')
                         bot = None
                         retries += 1
                 else:
+                    self.console.info(f'reached maximum retry limit for proxy {self.proxy}')
                     retries = 0
                     proxyNo+=1    
         bot = None
         self.console.info('goodbye!')
 
-    def loadFromFile(self, filename):
-        if filename:
-            try:
-                with open(filename, 'r') as file:
-                    proxyList = file.readlines()
-                f = "\n".join([x for x in proxyList])
-                ex = "(((25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|[1-9])\.){3}(25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|[1-9])|(\w*\.\w*\.\w*))\:(6553[0-5]|655[0-2]\d|65[0-4]\d\d|6[0-4]\d\d\d|[1-5]\d\d\d|[1-9]\d\d\d|[1-9]\d\d|[1-9]\d|[1-9])(\:.*\:.*)?"
-                if re.match(ex,f):
-                    self.console.info(f'proxies found {f}')
-                    return proxyList
-        
-            except FileNotFoundError:
-                self.console.error('cannot find proxy file; using localhost')
-                return [None]
-        self.console.info('no proxies provided; using localhost')
-        return [None]
+    def loadFromFile(self, filename, ex):
+
+        lst = []
+        try:
+            with open(filename, 'r') as file:
+                out = file.readlines()
+            for x in out:
+                if re.match(ex,x):
+                    lst.append(x)
+            return lst
+    
+        except FileNotFoundError:
+            pass
+            self.console.error(f'cannot find file {filename}!')
+            return None
+
     
     def loadProxy(self, proxy):
         split = proxy.split(':')
@@ -336,24 +355,27 @@ class BotManager():
             return split[0], split[1], split[2], split[3]
         elif len(split) == 2:
             return split[0], split[1], None, None
-        
+
+##TO-DO: change these setting to be stored in a config file    
 if __name__ == "__main__" :
 
     # filename = None
     # filename = str(input("filename (of proxies): "))
-    filename = 'proxies.txt'
-
-    dictionaries = [
-        "https://raw.githubusercontent.com/YoungsterGlenn/bpDictionaryStatistics/master/dictionary.txt",
-        "https://norvig.com/ngrams/sowpods.txt", 
-        "https://norvig.com/ngrams/enable1.txt",
-        "https://pastebin.com/raw/UegdKLq8",
-        "https://pastebin.com/raw/M6TZ8Uxy",
-        ]
+    # proxies = None
+    proxies = 'proxies.config'
+    # dictionaries = str(input("filename (of dictionaries): "))
+    dictionaries = 'dictionaries.txt'
     link = str(input("paste code: ")).upper()
     name = str(input("username: "))
 
+    if proxies == '':
+        proxies = None
+    if dictionaries == '':
+        dictionaries = None
+    if link == '':
+        link = None
+    if name == '':
+        name = None
 
-    # manager = BotManager(dictUrls=dictionaries, roomCode=link, username=name)
-    manager = BotManager(dictUrls=dictionaries, roomCode=link, proxyFile=filename, username=name)
+    manager = BotManager(dictFile=dictionaries, roomCode=link, proxyFile=proxies, username=name)
     manager.persistLoop()
