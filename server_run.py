11fb105b3ea5c54a8619dfe01b10c6c980c3586b
Eric Chen <aivectorec@gmail.com>
2026-09-13 23:13:11 -0400

    updated server_run.py for deployment

--- server_run.py
+++ server_run.py
@@ -1,6 +1,12 @@
 from app import BackendServer
+import os
+# if __name__ == '__main__':
+#     # Runs web server locally on http://127.0.0.1:5000
+#     server = BackendServer(port=5000, force_https=False)#false in test
+#     server.run()
 
-if __name__ == '__main__':
-    # Runs web server locally on http://127.0.0.1:5000
-    server = BackendServer(port=5000, force_https=False)#false in test
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", "10000"))
+    server = BackendServer(port=port, force_https=False)
     server.run()
